bb78658318d8bba4714de053c286fe3340edb84a
jeong-sik <yousleepwhen@gmail.com>
2026-01-23 14:24:35 +0900

    포맷 보정

--- test/test_server.ml
+++ test/test_server.ml
@@ -7,7 +7,8 @@ let echo_handler request =
 
 let () =
   Printf.printf "Starting grpc-direct test server on port 9940...\n%!";
-  let service = Grpc_eio.Service.create "test.EchoService"
+  let service =
+    Grpc_eio.Service.create "test.EchoService"
     |> Grpc_eio.Service.add_unary "Echo" echo_handler
   in
   let config : Grpc_eio.Server.config =
